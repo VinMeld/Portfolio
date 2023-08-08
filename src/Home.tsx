de7a6931b16ee9662433f0b14f24da2478ac0503
Vinay M <36042425+VinMeld@users.noreply.github.com>
2023-08-08 20:07:20 +0000

    Playing around with colours

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { MainContext } from "./App";  // make sure the import path is correct
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   const context = useContext(MainContext);
@@ -11,11 +12,47 @@ export default function Home() {
   const { theme, toggleTheme } = context;
 
   return (
-    <main className={`flex items-center justify-center h-screen w-screen ${theme} text-foreground bg-background`}>
-      <article>
-        <p color="primary">The current theme is {theme}.</p>
-        <button onClick={toggleTheme} color="primary">Toggle Theme</button>
-      </article>
-    </main>
+    <article className="p-6 space-y-6">
+      <h2 className="text-2xl font-semibold mb-4">
+        Welcome to the {theme.charAt(0).toUpperCase() + theme.slice(1)} Forest Theme!
+      </h2>
+      <p className="text-primary">The current theme is {theme}.</p>
+      
+      <div className="flex gap-4 items-center">
+        <Button size="sm" color="primary">
+          Small Primary
+        </Button>  
+        <Button size="md" color="secondary">
+          Medium Secondary
+        </Button>  
+        <Button size="lg" color="success">
+          Large Success
+        </Button>  
+      </div>
+
+      <div className="flex flex-wrap gap-4 items-center">
+        <Button color="default">
+          Default
+        </Button>
+        <Button color="primary">
+          Primary
+        </Button>  
+        <Button color="secondary">
+          Secondary
+        </Button>  
+        <Button color="success">
+          Success
+        </Button>  
+        <Button color="warning">
+          Warning
+        </Button>  
+        <Button color="danger">
+          Danger
+        </Button>
+        <Button onClick={toggleTheme}>
+          Toggle Theme
+        </Button>
+      </div>
+    </article>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,42 @@ export default {
   },
   darkMode: "class",
   plugins: [
-    nextui({}),
+    nextui({
+      prefix: "nextui",
+      addCommonColors: false,
+      defaultTheme: "light",
+      defaultExtendTheme: "light",
+      layout: {},
+      themes: {
+        light: {
+          layout: {
+            borderRadius: "6px",
+            boxShadow: "0 4px 6px rgba(100, 120, 140, 0.1)",
+          },
+          colors: {
+            primary: "#5D93E1",
+            secondary: "#FFD369",
+            background: "#E9ECEF",
+            foreground: "#333",
+            border: "#D0D9D9",
+            muted: "#B0BAC9",
+          },
+        },
+        dark: {
+          layout: {
+            borderRadius: "6px",
+            boxShadow: "0 4px 6px rgba(40, 42, 49, 0.2)",
+          },
+          colors: {
+            primary: "#B088F9",
+            secondary: "#4D80E4",
+            background: "#282A31",
+            foreground: "#E9ECEF",
+            border: "#3C3F48",
+            muted: "#737581",
+          },
+        },
+      },
+    }),
   ]
 }
