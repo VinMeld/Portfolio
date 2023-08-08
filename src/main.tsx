69e34b413a99a78287162219d4b93a9b6a8e65aa
Vinay M <36042425+VinMeld@users.noreply.github.com>
2023-08-08 19:17:43 +0000

    Added non null assertion

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 import "./index.css";
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
       <main className="dark text-foreground bg-background">
