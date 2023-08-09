f456884743a234f0131cb3cfceccdc730be538cb
vinaycat <vinaymeldrum@gmail.com>
2023-08-09 17:12:42 -0400

    Changed icon to be in left

--- src/App.tsx
+++ src/App.tsx
@@ -27,16 +27,16 @@ export default function App() {
         className={`min-h-screen ${theme} font-inter text-foreground bg-background`}
       >
       <Button
-        type="button"
-        onClick={toggleTheme}
-        className="fixed p-2 z-1 right-2 sm:right-10 top-4 text-lg p-1 rounded-sm bg-transparent"
-        color="primary"
+          type="button"
+          onClick={toggleTheme}
+          className="relative p-2 z-1 left-2 sm:left-10 sm:fixed top-4 text-lg p-1 rounded-sm bg-transparent"
+          color="primary"
       >
-        <Image
-          width={25}
-          alt="Sun or Moon"
-          src={theme === "light" ? `./sun.svg` : `./moon.svg`}
-        />
+          <Image
+              width={25}
+              alt="Sun or Moon"
+              src={theme === "light" ? `./sun.svg` : `./moon.svg`}
+          />
       </Button>
         <div className="max-w-5xl w-11/12 mx-auto">
           <Intro />
